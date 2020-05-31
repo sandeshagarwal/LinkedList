4d0baf8d05cbf9073169d29ce726f59513571d61
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-05-31 21:40:52 +0530

    P4 - This code also works

---
+++ Program 4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class test{
+    int x;
+    test(int z){
+        x = z;
+        System.out.println(x);
+    }
+}
+public class Main{
+  public static void main (String[] args) {
+        test obj = new test(10);
+        obj = new test(20);
+    }
+}
